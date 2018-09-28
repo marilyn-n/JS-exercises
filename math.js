052392d7c565b8fdf4f545addc9e3d7be716074f
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-09-28 13:52:06 -0500

    create randomWords exercise

--- math.js
+++ math.js
@@ -8,3 +8,20 @@ const insultPart2 = animals[Math.floor(Math.random() * animals.length)]
 const insult = `You are a ${insultPart1} ${insultPart2}`
 
 console.log(insult)
+
+
+// Extend the random insult generator so it generates insults like 
+// “Your [body part] is more [adjective] than a [animal]’s [animal body part].” 
+// (Hint: You’ll need to create another array.)
+
+
+const bodyWords = ['head', 'neck', 'arm', 'hair', 'leg', 'mouth', 'nose']
+const adjectives = ['larger', 'furry', 'pretty', 'big']
+const animals2 = ['bear', 'pussy', 'horse', 'pig', 'wale']
+const animalBodyParts = ['ears', 'tale', 'paw', 'horn']
+
+const randomWords = arr => arr[Math.floor(Math.random() * arr.length)]
+
+const str = `Your ${randomWords(bodyWords)} is more ${randomWords(adjectives)} than a ${randomWords(animals2)}’s ${randomWords(animalBodyParts)}.`
+
+console.log(str)
