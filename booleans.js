6bdb1b4578c9cf2ea9f66a5c23143a188cd750b0
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-09-30 13:39:11 -0500

    add booleans.js

---
+++ booleans.js
@@ -0,0 +1,17 @@
+const name = 'Rafael'
+
+if(name === 'Marilyn') {
+  console.log('Hello me!')
+} else {
+  console.log('Hello stranger!')
+}
+
+const parentName = 'Claudia'
+
+if(parentName === 'Jose') {
+  console.log('Hi dad!')
+} else if (parentName === 'Claudia') {
+  console.log('Hi mom!')
+} else {
+  console.log('Hi stranger!')
+}
